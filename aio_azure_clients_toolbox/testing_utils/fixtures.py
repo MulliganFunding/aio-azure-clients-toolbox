783f940251cdec4f6b2315e67251ce677934fc53
Erik Aker <eraker@gmail.com>
2026-04-29 16:17:04 -0700

    fix the thing

--- aio_azure_clients_toolbox/testing_utils/fixtures.py
+++ aio_azure_clients_toolbox/testing_utils/fixtures.py
@@ -5,7 +5,7 @@ from azure.cosmos import aio as az_cosmos_client
 from azure.eventgrid import EventGridPublisherClient
 from azure.eventgrid.aio import EventGridPublisherClient as AsyncEventGridPublisherClient
 from azure.eventhub.aio import EventHubProducerClient
-from azure.identity import DefaultAzureCredential
+from azure.identity.aio import DefaultAzureCredential
 from azure.servicebus.aio import ServiceBusClient
 from azure.storage.blob.aio import BlobClient, BlobServiceClient, ContainerClient
 
